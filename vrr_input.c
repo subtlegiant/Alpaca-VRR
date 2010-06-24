93b84e564bcb0b0f7ec0b2cbadaa00a7cc75d4c8
Cameron Kidd <cameron.kidd@gmail.com>
2010-06-23 20:20:40 -0700

    Added header access functionality to core

--- vrr_input.c
+++ vrr_input.c
@@ -2,19 +2,16 @@
 #include "linux/skbuff.h"
 #include "linux/netdevice.h"
 
-static int vrr_active = 0;  //Activates after first route successfully set up
+static int vrr_active = 0;	//Activates after first route successfully set up
 
 //Struct for use in VRR Routing Table
-typedef struct routing_table_entry
-{
-    u_int ea;     //endpoint A
-    u_int eb;     //endpoint B
-    u_int na;     //next A
-    u_int nb;     //next B
-    int path_id //Path ID
+typedef struct routing_table_entry {
+	u_int ea;		//endpoint A
+	u_int eb;		//endpoint B
+	u_int na;		//next A
+	u_int nb;		//next B
+	int path_id		//Path ID
 } rt_entry;
-
-
 
 int vrr_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *pt,
 	    struct net_device *orig_dev)
@@ -22,47 +19,45 @@ int vrr_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *pt,
 	/* Do stuff! */
 	printk("Received a VRR packet!");
 
-	//int pt = get packet type out of packet
-
+	int pt = get_pkt_type(skb);
 	/*
-	if (pt == 0) { //Data
-		u_int nh = NextHop(rt, dst)		//find next hop
-		if nh == 0
-			send to application layer
-		else
-			send packet back out to nh
-	}
-	else if (pt == 1) { //Hello
-		if src is in pset
-			do nothing
-		else if (me == active)
-			Need to understand 3.3.1, 3.3.2 and 3.3.3 text
-		else	//proxy found
-			send setup_req packet to proxy	
-	}
-	else if (pt == 2) { //Setup Request
-		send Setup packet to src from me, and include my vset
-		add src to vset
-	}
-	else if (pt == 3) { //Setup
-		add src to vset
-		get vset' from packet
-		send setup_req to all nodes in vset'
-		once all setups received from further sent setup_req, then activate me
-	}
-	else if (pt == 4) { //Setup Fail
+	   if (pt == 0) { //Data
+	   u_int nh = NextHop(rt, dst)          //find next hop
+	   if nh == 0
+	   send to application layer
+	   else
+	   send packet back out to nh
+	   }
+	   else if (pt == 1) { //Hello
+	   if src is in pset
+	   do nothing
+	   else if (me == active)
+	   Need to understand 3.3.1, 3.3.2 and 3.3.3 text
+	   else //proxy found
+	   send setup_req packet to proxy       
+	   }
+	   else if (pt == 2) { //Setup Request
+	   send Setup packet to src from me, and include my vset
+	   add src to vset
+	   }
+	   else if (pt == 3) { //Setup
+	   add src to vset
+	   get vset' from packet
+	   send setup_req to all nodes in vset'
+	   once all setups received from further sent setup_req, then activate me
+	   }
+	   else if (pt == 4) { //Setup Fail
 
-	}
-	else if (pt == 5) { //Teardown
+	   }
+	   else if (pt == 5) { //Teardown
 
-	}
-	else {
-		//drop packet?
-	}
+	   }
+	   else {
+	   //drop packet?
+	   }
 
-	*/
+	 */
 }
-
 
 /*DEFINITIONS
  * me = local node
@@ -72,7 +67,6 @@ int vrr_rcv(struct sk_buff *skb, struct net_device *dev, struct packet_type *pt,
  * rt = routing table
  */
 
-
 //Psuedo Code for more functions
 /*
 NextHop (rt, dst)
@@ -80,7 +74,6 @@ NextHop (rt, dst)
     if (endpoint == me)
         return null
     return next hop towards endpoint in rt
-
 
 Receive (<setup_req,src,dst,proxy,vset'>, sender)
     nh := NextHopExclude(rt, dst, src)
@@ -92,7 +85,6 @@ Receive (<setup_req,src,dst,proxy,vset'>, sender)
             Send setup, me, src, NewPid(),proxy, ovset to me
         else
             Send setup fail, me, src, proxy, ovset to me
-
 
 Receive (<setup,src,dst,proxy,vset'>, sender)
     nh := (dst in pset) ? dst : NextHop(rt, proxy)
@@ -108,14 +100,12 @@ Receive (<setup,src,dst,proxy,vset'>, sender)
     else
         TearDownPath( pid, src , null)
 
-
 Receive (<setup_fail,src,dst,proxy,vset'>, sender)
     nh := (dst ∈ pset) ? dst : NextHop(rt, proxy)
     if (nh = null)
         Send setup fail, src, dst, proxy, vset’ to nh
     else if (dst = me)
         Add(vset, null, vset’ Union {src} )
-
 
 Receive (<teardown, <pid,ea>, vset'>, sender)
     < ea , eb , na , nb , pid := Remove(rt, pid, ea )
@@ -131,7 +121,6 @@ Receive (<teardown, <pid,ea>, vset'>, sender)
             proxy := PickRandomActive(pset)
             Send <setup_req, me, e, proxy, vset> to proxy
 
-
 Add (vset, src, vset')
     for each (id ∈ vset’)
         if (ShouldAdd(vset, id))
@@ -143,7 +132,6 @@ Add (vset, src, vset')
         return true;
     return false;
 
-
 TearDownPath(<pid,ea>, sender)
     < ea , eb , na , nb , pid := Remove(rt, pid, ea )
     for each (n ∈ {na , nb , sender})
@@ -151,39 +139,31 @@ TearDownPath(<pid,ea>, sender)
             vset’ := (sender = null) ? vset : null
             Send teardown, pid, ea , vset’ to n
 
-
 NextHopExclude (rt, dst, src)
     endpoint := closest id to dst from Endpoints(rt) that is not src
     if (endpoint == me)
         return null
     return next hop towards endpoint in rt
 
-
 NewPid()
 	returns a new path identifier that is not in use by the local node
-
 
 Add(rt, <ea , eb , na , nb , pid> )
 	adds the entry to the routing table unless there is already an
 	entry with the same pid, ea 
 
-
 Remove(rt, <pid, ea> )
 	removes and returns the entry identified by pid, ea from the routing table
-
 
 PickRandomActive(pset)
 	returns a random physical neighbor that is active
 
-
 Remove(vset, id)
 	removes node id from the vset
-
 
 ShouldAdd(vset, id)
 	sorts the identifiers in vset union {id, me} and returns true if id
 	should be in the vset;
-
 
 TearDownPathTo(id)
 	similar to TearDownPath but it tears down all vset-paths that have
